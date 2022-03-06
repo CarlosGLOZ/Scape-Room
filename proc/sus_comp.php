073cb92894f4cbc2e9b9c0207c865c21cfada54c
CalosGLOZ <cgiraldolozano@gmail.com>
2022-03-06 11:04:06 +0100

    backend acabado fix

--- proc/sus_comp.php
+++ proc/sus_comp.php
@@ -1,7 +1,7 @@
 <?php
     $sospechoso = $_POST['sus'];
 
-    if ($sospechoso == "larry_butz") {
+    if ($sospechoso == "Klavier Gavin") {
         header("Location: ../pant_finales/ganada.php");
     } else {
         header("Location: ../pant_finales/perdida.php");
